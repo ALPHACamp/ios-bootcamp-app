ba4d629a36fb1e31ee2cf5aafdbaf599aa01f208
PiHanHsu <pihanhsu@gmail.com>
2015-02-14 16:31:46 +0800

    Commit Login with API

--- ALPHACampApp/ViewControllers/LoginViewController.m
+++ ALPHACampApp/ViewControllers/LoginViewController.m
@@ -8,11 +8,15 @@
 
 #import "LoginViewController.h"
 #import <Parse/Parse.h>
+#import <AFNetworking.h>
+
+#define api_key @"21f7814731bbbcc3302fbe06194e53c4993a3976"
 
 @interface LoginViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (strong, nonatomic)NSString * auth_token;
 
 @end
 
@@ -30,48 +34,86 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)loginButtonPredded:(id)sender {
-    
-    [PFUser logInWithUsernameInBackground:self.emailTextField.text password:self.passwordTextField.text
-                                    block:^(PFUser *user, NSError *error) {
-                                        if (user) {
-                                            UITabBarController *tabBarVC = [self.storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
-                                            [tabBarVC setSelectedIndex:0];
-                                            [self presentViewController:tabBarVC animated:YES completion:nil];
 
-                                        } else {
-                                            UIAlertController * alert=   [UIAlertController
-                                                                          alertControllerWithTitle:@"帳號密碼錯誤，請重新輸入"
-                                                                          message:@"請注意是否大小寫有誤"
-                                                                          preferredStyle:UIAlertControllerStyleAlert];
-                                            
-                                            UIAlertAction* ok = [UIAlertAction
-                                                                 actionWithTitle:@"OK"
-                                                                 style:UIAlertActionStyleDefault
-                                                                 handler:^(UIAlertAction * action)
-                                                                 {
-                                                                     self.emailTextField.text=@"";
-                                                                     self.passwordTextField.text=@"";
-                                                                 }];
-                                            
-                                            [alert addAction:ok];
-                                            
-                                            [self presentViewController:alert animated:YES completion:nil];
-                   }
-                                    }];
-    
-    
-    
-   }
+#pragma mark - Login with API
+- (IBAction)loginButtonPressed:(id)sender {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    NSDictionary *parameters = @{
+                                 @"email": self.emailTextField.text,
+                                 @"password": self.passwordTextField.text};
+    [manager POST:@"https://school.alphacamp.co/api/v1/login"
+       parameters:parameters
+          success:^(AFHTTPRequestOperation *operation, id responseObject) {
+              
+              UITabBarController *tabBarVC = [self.storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
+              [tabBarVC setSelectedIndex:0];
+              [self presentViewController:tabBarVC animated:YES completion:nil];
 
-/*
-#pragma mark - Navigation
+              
+              NSLog(@"JSON: %@", responseObject);
+              
+              self.auth_token = [NSString stringWithFormat:@"%@", responseObject[@"auth_token"]];
+          } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+              NSLog(@"Login in Error: %@", error);
+              //NSString *erroeMessage = [NSString stringWithFormat:@"%@", error];
+              UIAlertController *alert= [UIAlertController alertControllerWithTitle:@"Login failed!!"
+                                   message:nil
+                            preferredStyle:UIAlertControllerStyleAlert];
+              
+              UIAlertAction* ok = [UIAlertAction
+                                   actionWithTitle:@"OK"
+                                             style:UIAlertActionStyleDefault
+                                           handler:^(UIAlertAction * action){
+                                            self.emailTextField.text=@"";
+                                            self.passwordTextField.text=@"";
+                                           }];
+              [alert addAction:ok];
+              [self presentViewController:alert animated:YES completion:nil];
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+          }];
+
+    
 }
-*/
+
+
+
+
+
+#pragma mark - Parse Login
+//- (IBAction)loginButtonPressed:(id)sender {
+//    
+//    [PFUser logInWithUsernameInBackground:self.emailTextField.text password:self.passwordTextField.text
+//                                    block:^(PFUser *user, NSError *error) {
+//                                        if (user) {
+//                                            UITabBarController *tabBarVC = [self.storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
+//                                            [tabBarVC setSelectedIndex:0];
+//                                            [self presentViewController:tabBarVC animated:YES completion:nil];
+//
+//                                        } else {
+//                                            UIAlertController * alert=   [UIAlertController
+//                                                                          alertControllerWithTitle:@"帳號密碼錯誤，請重新輸入"
+//                                                                          message:@"請注意是否大小寫有誤"
+//                                                                          preferredStyle:UIAlertControllerStyleAlert];
+//                                            
+//                                            UIAlertAction* ok = [UIAlertAction
+//                                                                 actionWithTitle:@"OK"
+//                                                                 style:UIAlertActionStyleDefault
+//                                                                 handler:^(UIAlertAction * action)
+//                                                                 {
+//                                                                     self.emailTextField.text=@"";
+//                                                                     self.passwordTextField.text=@"";
+//                                                                 }];
+//                                            
+//                                            [alert addAction:ok];
+//                                            
+//                                            [self presentViewController:alert animated:YES completion:nil];
+//                   }
+//                                    }];
+//    
+//    
+//    
+//   }
+
+
 
 @end
